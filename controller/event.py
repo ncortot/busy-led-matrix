629ae31f56715e81e1e03f62031efca28717d498
Guillaume Bandet <guillaume.bandet@m4x.org>
2016-10-20 15:14:24 +0200

    Controller that reads keyboard events to change images.

---
+++ controller/event.py
@@ -0,0 +1,22 @@
+import evdev
+from evdev.util import categorize
+import os
+
+
+device = evdev.InputDevice('/dev/input/event0')
+
+keymap = {
+    'KEY_KP1': 'images/open.png',
+    'KEY_KP2': 'images/closed.png',
+    'KEY_KP3': 'images/pacman.png',
+    'KEY_KP4': 'images/street.png',
+    'KEY_KP5': 'images/call.png',
+    'KEY_KP7': 'images/beer.png',
+}
+
+for event in device.read_loop():
+    if event.type == evdev.ecodes.EV_KEY:
+        key = evdev.events.KeyEvent(event)
+        if key.keycode in keymap and key.keystate == key.key_down:
+            command = "( echo -ne '\\xff'; convert %s -rotate 90 -depth 1 rgba:-; echo -ne '\\xff' ) > /dev/ttyAMA0" % keymap[key.keycode]
+            os.system(command)
